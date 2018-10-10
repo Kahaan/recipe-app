a33bd3ad04b4a8f6e2e6957e1d78147289a880d4
Kahaan <kahaan.r.patel@gmail.com>
2018-10-10 02:09:59 -0700

    api setup

--- src/form.js
+++ src/form.js
@@ -6,9 +6,9 @@ import React from 'react'
 
 const Form = (props) => {
   return (
-    <form onSubmit={props.getRecipe}>
-      <input type="text" name="recipeName"></input>
-      <button>Search</button>
+    <form onSubmit={props.getRecipe} style={{marginBottom:"2rem"}} >
+      <input className="form__input" type="text" name="recipeName"></input>
+      <button className="form__button">Search</button>
     </form>
   )
 }
