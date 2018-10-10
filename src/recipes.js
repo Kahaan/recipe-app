1c23118348c70b7a4744ab7cab197f2b6cd6b8f4
Kahaan <kahaan.r.patel@gmail.com>
2018-10-10 02:29:44 -0700

    api setup

--- src/recipes.js
+++ src/recipes.js
@@ -3,15 +3,25 @@ import React from 'react'
 
 const Recipes = (props) => {
   return (
-    <div>
-    {props.recipes.map((el) => {
-      return(
-        <div>
-          <img key={el.title} src={el.image_url} alt={el.title} />
-          <p>{el.title}</p>
-        </div>
-      )
-    })}
+    <div className="container">
+      <div className="row">
+        {props.recipes.map((el) => {
+          return(
+            <div className="col-md-4" key={el.title} style={{marginBottom:"2rem"}}>
+              <div className="recipes__box">
+                <img className="recipe__box-img" src={el.image_url} alt={el.title} />
+                <div className="recipe__text">
+                  <h5 className="recipes__title">{el.title}</h5>
+                  <p className="recipes__subtitle">Publisher: <span>
+                    {el.publisher}
+                  </span></p>
+                </div>
+                <button className="recipe_buttons">View recipes</button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
